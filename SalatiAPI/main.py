17cf7f3420c15bba587f56b4cba61dbd18867c65
kidreveloution <alyeldinshahin@gmail.com>
2022-02-20 02:05:54 -0500

    Fixed some bugs, handled API through HTML
    Today was a big day, full app is done, I just need to deploy on a live server. And make it pretty

--- SalatiAPI/main.py
+++ SalatiAPI/main.py
@@ -4,10 +4,23 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from mainDriver import mainFunc
 from fastapi.responses import FileResponse
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "file:///C:/Users/aladdin/Desktop/ahlan-salati/FrontEnd/Testing.html"
+    " http://192.168.50.189:8080",
+    "http://127.0.0.1:8080"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def read_item():
@@ -15,6 +28,6 @@ async def read_item():
 
 
 @app.get("/latLong/")
-async def read_item(lat: int,long:int):
+async def read_item(lat: float,long:float):
     mainFunc(lat,long)
-    return FileResponse("/tmp/mySalat.ics")
+    return FileResponse ("/tmp/mySalat.ics",media_type="mySalat/ics",filename="mySalat.ics")
